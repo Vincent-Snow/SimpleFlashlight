5a068301456c0244f515655ed00ec3c164910298
Vincent-Snow <vincent.van.snow@gmail.com>
2021-01-05 12:11:23 -0700

    just having fun
    not really a flashlight anymore

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -12,26 +12,36 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var turnOn: UIButton!
     var isOn: Bool = true
-    
+    let colors: [UIColor] = [.blue, .purple, .red, .yellow, .brown, .clear, .cyan, .green, .darkGray, .magenta]
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
-    @IBAction func turnOnClicked(_ sender: Any) {
-        if isOn == true {
-            isOn = false
-            view.backgroundColor = .purple
-            turnOn.setTitle("Turn On", for: .normal)
-            turnOn.setTitleColor(.white, for: .normal)
-            
+    var counter = 0
+    func clicked() {
+        view.backgroundColor = colors[counter]
+        if counter < colors.count-1 {
+            counter+=1
         } else {
-            isOn = true
-            view.backgroundColor = .blue
-            turnOn.setTitle("Turn Off", for: .normal)
-            turnOn.setTitleColor(.black, for: .normal)
-            
+            counter = 0
         }
+    }
+    
+    @IBAction func turnOnClicked(_ sender: Any) {
+            clicked()
+//        if isOn == true {
+//            isOn = false
+//            view.backgroundColor = .purple
+//            turnOn.setTitle("Turn On", for: .normal)
+//            turnOn.setTitleColor(.white, for: .normal)
+//
+//        } else {
+//            isOn = true
+//            view.backgroundColor = .blue
+//            turnOn.setTitle("Turn Off", for: .normal)
+//            turnOn.setTitleColor(.black, for: .normal)
+//
+//        }
     }
 
 }
